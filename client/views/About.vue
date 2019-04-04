fceaa34d81dd7bc5cffdc3bb0b16f41154cae88f
Renato Neto <rnato.netoo@gmail.com>
2019-04-04 00:40:47 -0300

    about style

--- client/views/About.vue
+++ client/views/About.vue
@@ -1,17 +1,21 @@
 <template>
-  <div>
-    <h1>Quem somos</h1>
-    <p>A Oito nasceu do desejo de conectar pessoas e oportunidades. Profissionais autônomos divulgam seus serviços e se conectam com clientes que procuram soluções. Tudo de forma clara, rápida, justa e proveitosas para os dois lados.</p>
-    <p>Enxergamos a necessidade de milhões de brasileiros de encontrar oportunidades no mercado de trabalho, por isso criamos uma tecnologia que ranqueia e qualifica profissionais e oportunidades, agilizando e certificando o processo de contratação e divulgação.</p>
-    <h1>Missão</h1>
-    <p>Queremos catalisar o processo de contratação baseado em ranqueamento para pessoas, gerando conexões que sejam mutuamente proveitosas.</p>
-    <h1>Visão</h1>
-    <p>Ser protagonista no processo de inclusão social, por meio de conexões que sejam mutuamente proveitosas, transparentes e justas.</p>
-    <h1>Valores</h1>
-    <p>Transparência: baseado no ranqueamento, todas as relações geradas por meio da OiTO possuem um índice de qualidade livre para ser alterado mediante feedback.</p>
-    <p>Agilidade: gerar oportunidades de forma rápida, prática e objetiva tanto para quem oferece quanto para quem procura, minimizando dificuldades e maximizando soluções.</p>
-    <p>Responsabilidade social: contribuir para que o autônomo alcance liberdade e estabilidade financeira, por meio de divulgação de seus serviços.</p>
-  </div>
+  <v-container fluid fill-height>
+    <v-layout justify-center align-center>
+      <v-flex text-xs-left>
+        <h1>Quem somos</h1>
+        <p>A Oito nasceu do desejo de conectar pessoas e oportunidades. Profissionais autônomos divulgam seus serviços e se conectam com clientes que procuram soluções. Tudo de forma clara, rápida, justa e proveitosas para os dois lados.</p>
+        <p>Enxergamos a necessidade de milhões de brasileiros de encontrar oportunidades no mercado de trabalho, por isso criamos uma tecnologia que ranqueia e qualifica profissionais e oportunidades, agilizando e certificando o processo de contratação e divulgação.</p>
+        <h1>Missão</h1>
+        <p>Queremos catalisar o processo de contratação baseado em ranqueamento para pessoas, gerando conexões que sejam mutuamente proveitosas.</p>
+        <h1>Visão</h1>
+        <p>Ser protagonista no processo de inclusão social, por meio de conexões que sejam mutuamente proveitosas, transparentes e justas.</p>
+        <h1>Valores</h1>
+        <p>Transparência: baseado no ranqueamento, todas as relações geradas por meio da OiTO possuem um índice de qualidade livre para ser alterado mediante feedback.</p>
+        <p>Agilidade: gerar oportunidades de forma rápida, prática e objetiva tanto para quem oferece quanto para quem procura, minimizando dificuldades e maximizando soluções.</p>
+        <p>Responsabilidade social: contribuir para que o autônomo alcance liberdade e estabilidade financeira, por meio de divulgação de seus serviços.</p>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -19,3 +23,9 @@ export default {
   name: "About"
 };
 </script>
+
+<style scoped>
+h1 {
+  color: #EF974F;
+}
+</style>
